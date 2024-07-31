216fca4c0de31ef0b561092689a88bb3ae50dda7
sadaam009 <sadaamdahir090@gmail.com>
2024-07-31 10:18:04 +0100

    sadaam commit

---
+++ src/main/java/com/example/Midowga_DhalinyaradaDB/MidowgaDhalinyaradaDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.Midowga_DhalinyaradaDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MidowgaDhalinyaradaDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MidowgaDhalinyaradaDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Midowga_DhalinyaradaDB/MidowgaDhalinyaradaDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Midowga_DhalinyaradaDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MidowgaDhalinyaradaDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
