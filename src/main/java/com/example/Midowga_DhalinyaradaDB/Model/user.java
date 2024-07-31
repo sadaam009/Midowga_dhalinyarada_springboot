45691f91adb4e82a4eeb698755f45e71ee610d4f
sadaam009 <sadaamdahir090@gmail.com>
2024-07-31 10:28:29 +0100

    Waxaa ku soo daray user class

--- src/main/java/com/example/Midowga_DhalinyaradaDB/Model/user.java
+++ src/main/java/com/example/Midowga_DhalinyaradaDB/Model/user.java
@@ -1,5 +1,26 @@
 package com.example.Midowga_DhalinyaradaDB.Model;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "macmiil")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class user {
-    private Integer id;
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String phone;
+    private String degan;
+    private String stateedu;
+    private String jop;
+    private String bloodtype;
 }
